9bd7326c1e3ba236126a37727bcf634636861c95
Badriddin Nazhmudinov <bnazhmudinov@Noutbuk-Badriddin.local>
2024-08-07 16:39:21 +0500

    fix

--- product/models.py
+++ product/models.py
@@ -37,30 +37,3 @@ class CartItem(models.Model):
 
     def __str__(self):
         return f'Cart: {self.id}-{self.quantity}-{self.product.price}'
-
-
-class Order(models.Model):
-    STATUS_CHOICES = [
-        ('processing', 'В обработке'),
-        ('shipped', 'Отправлен'),
-        ('delivered', 'Доставлен'),
-        ('cancelled', 'Отменен'),
-    ]
-    
-    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    total_price = models.DecimalField(max_digits=10, decimal_places=2)
-    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='processing')
-
-    def __str__(self):
-        return f'Order {self.id} by {self.user.username}'
-    
-
-class OrderItem(models.Model):
-    order = models.ForeignKey(to=Order, on_delete=models.CASCADE, related_name='order_item')
-    product = models.ForeignKey(to=Product, on_delete=models.CASCADE, related_name='order_product')
-    quantity = models.PositiveIntegerField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-
-    def __str__(self):
-        return f'Order: {self.id}-{self.quantity}-{self.price}'
