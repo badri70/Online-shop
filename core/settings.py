67a426259f13db0f03a3ee076e6e9840ea5befdd
Badriddin Nazhmudinov <bnazhmudinov@Noutbuk-Badriddin.local>
2024-08-13 01:21:00 +0500

    Added docker-compose file.

--- core/settings.py
+++ core/settings.py
@@ -86,6 +86,14 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': config('POSTGRES_DB'),
+    #     'USER': config('POSTGRES_USER'),
+    #     'PASSWORD': config('POSTGRES_PASSWORD'),
+    #     'HOST': 'localhost',
+    #     'PORT': '5432'
+    # }
 }
 
 
