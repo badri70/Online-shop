3215488c9356b2956ffbd6231e8d1d19d7c17e9b
Badriddin Nazhmudinov <107610761+badri70@users.noreply.github.com>
2025-10-04 22:31:48 +0500

    Update wait_for_db.sh

--- wait_for_db.sh
+++ wait_for_db.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
-
 echo "Waiting for postgres..."
-
 while ! nc -z db 5432; do
   sleep 1
 done
-
 echo "PostgreSQL started"
-
-python manage.py migrate
-python manage.py runserver 0.0.0.0:8000
