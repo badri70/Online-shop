a75b8474b989454668ab6a65f5c6f7188f022f13
Badriddin Nazhmudinov <107610761+badri70@users.noreply.github.com>
2025-10-13 11:33:14 +0500

    Update tests.py

--- product/tests.py
+++ product/tests.py
@@ -3,6 +3,9 @@
 from django.test import TestCase
 from product.models import Category, Product
 from decimal import Decimal
+from django.test import Client
+from django.urls import reverse
+
 
 class CategoryModelTest(TestCase):
 
@@ -61,3 +64,63 @@ class ProductModelTest(TestCase):
         self.assertFalse(self.product.stock > 0)
 
 
+class ProductViewTest(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.category_a = Category.objects.create(name="A")
+        self.category_b = Category.objects.create(name="B")
+        
+        # Создаем несколько товаров
+        self.product_1 = Product.objects.create(
+            name="Товар 1",
+            description="Детали 1",
+            price=10.00,
+            stock=10,
+            category=self.category_a
+        )
+        self.product_2 = Product.objects.create(
+            name="Товар 2",
+            description="Детали 2",
+            price=20.00,
+            stock=5,
+            category=self.category_b
+        )
+        
+        # Предполагаемые URL-имена (из urls.py)
+        self.list_url = reverse('product:product_list')
+        self.detail_url = reverse('product:product_detail', args=[self.product_1.id])
+        # Добавим тест на просмотр по категории, если он есть
+        self.category_url = reverse('product:category_view', args=[self.category_a.id]) 
+
+
+    def test_product_list_view_status_code(self):
+        """Проверяет, что страница со списком товаров доступна."""
+        response = self.client.get(self.list_url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_product_list_view_contains_all_products(self):
+        """Проверяет, что страница списка содержит оба товара."""
+        response = self.client.get(self.list_url)
+        self.assertContains(response, "Товар 1")
+        self.assertContains(response, "Товар 2")
+        self.assertContains(response, self.category_a.name) # Проверяем, что отображается название категории
+
+    def test_product_detail_view_status_code(self):
+        """Проверяет, что страница с деталями товара доступна."""
+        response = self.client.get(self.detail_url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_product_detail_view_correct_content(self):
+        """Проверяет, что на странице деталей отображаются данные конкретного товара."""
+        response = self.client.get(self.detail_url)
+        self.assertContains(response, self.product_1.name)
+        self.assertContains(response, self.product_1.description)
+        self.assertNotContains(response, self.product_2.name) # Убеждаемся, что не видно другого товара
+        
+    def test_product_detail_view_404_on_invalid_id(self):
+        """Проверяет, что при неверном ID товара возвращается 404."""
+        # Ищем товар с ID, которого точно нет
+        invalid_url = reverse('product:product_detail', args=[9999]) 
+        response = self.client.get(invalid_url)
+        self.assertEqual(response.status_code, 404)
